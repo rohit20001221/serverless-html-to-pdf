7d898c1c9b5a957a55374c3ff21d84e5dc480386
rohit20001221 <rohit20001221@gmail.com>
2022-09-22 18:27:50 +0530

    post

--- api/html_to_pdf.py
+++ api/html_to_pdf.py
@@ -2,16 +2,32 @@ from http.server import BaseHTTPRequestHandler
 from xhtml2pdf import pisa
 from io import StringIO, BytesIO
 
+def getPdf(html):
+    pdf = BytesIO()
+    pisa.CreatePDF(StringIO(html), pdf)
+    data = pdf.getvalue()
+
+    return data
+
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type','application/pdf')
         self.end_headers()
 
+    def do_GET(self):
+        self._set_headers()
+
         html = "<h1>hello world</h1>"
-        pdf = BytesIO()
-        pisa.CreatePDF(html, pdf)
-        data = pdf.getvalue()
+        data = getPdf(html)
         self.wfile.write(data)
 
         return
+
+    def do_POST(self):
+        self._set_headers()
+
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        data = getPdf(post_body)
+        self.wfile.write(data)
