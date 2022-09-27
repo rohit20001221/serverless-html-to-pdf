29ab78b776c46161b786ff80e10e574a1988ed59
rohit20001221 <rohit20001221@gmail.com>
2022-09-27 11:45:48 +0530

    .

--- api/html_to_pdf_node.js
+++ api/html_to_pdf_node.js
@@ -1,7 +1,9 @@
 import pdf from "html-pdf"
 
 export default async function(req, res) {
-    pdf.create("<h1>Hello World</h1>").toBuffer(buffer => {
+    pdf.create("<h1>Hello World</h1>").toBuffer((err, buffer)=> {
+        console.log(buffer)
+        console.log(err)
         res.setHeader('Content-type','application/pdf')
         res.send(buffer)
     })
